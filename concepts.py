c247206e22a4b75322160d2e212e11c572984e67
nabinchowdhury <nabinchowdhury26@gmail.com>
2024-10-18 18:34:47 +0600

    concepts added

---
+++ concepts.py
@@ -0,0 +1,20 @@
+"""
+------------------------------------
+            HIGH LEVEL AREAS
+-------------------------------------
+School system management
+    1. Student management
+    2. Teacher management
+    3. Cafeteria
+    4. Sports management
+    5. Club management
+    6. Event management
+    7. Library management
+    8. Asset management
+-----------------------------
+Core Functionalities
+    1. A classroom consisting of multiple students
+    2. Students can enroll multiple classes
+    3. Teachers will take classes, exams, grade the exams 
+    4. System will generate the overall grade
+"""
